c5b712563da84f90a4d0e66c6386dcd49c551085
valeriva44 <106684206+valeriva44@users.noreply.github.com>
2022-07-05 11:32:23 -0300

    Add files via upload

---
+++ editar.php
@@ -0,0 +1,34 @@
+<?php
+
+include("con_db.php");
+$ID=$_POST['ID'];
+
+$Nombre=$_POST['Nombre'];
+$Mail=$_POST['Mail'];
+$Producto=$_POST['Producto'];
+
+
+
+$sql = "UPDATE clientes SET Producto='$Producto' WHERE mail='$Mail'";
+
+$ejecutar = mysqli_query($conex, $sql);
+
+   if($ejecutar) {
+       echo '
+       <script>
+           alert("Compra modificada exitosamente");
+           window.location= "index.php";
+       </script>
+
+       ';
+   } else { '
+       <script>
+           alert("Intentalo de nuevo, compra no modificada");
+           window.location= "index.php";
+       </script>
+     
+       ';
+   }
+   
+  mysqli_close($conex);
+?>
