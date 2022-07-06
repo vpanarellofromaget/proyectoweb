c6f4ca6e748a7716dc093dc1bccb3039d010bafa
valeriva44 <106684206+valeriva44@users.noreply.github.com>
2022-07-06 19:20:11 -0300

    Add files via upload

---
+++ catalogo.php
@@ -0,0 +1,284 @@
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Catalogo - Surreal</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+				<!-- Note: The "styleN" class below should match that of the banner element. -->
+					<header id="header" class="alt style2">
+						<a href="index.php" class="logo"><strong>Surreal</strong> <span></span></a>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+				<nav id="menu">
+						<ul class="links">
+							<li><a href="index.php">Inicio</a></li>
+							<li><a href="catalogo.php">Catalogo</a></li>
+							<li><a href="informacion.php">Informacion</a></li>
+							<li><a href="contacto.php">Contacto</a></li>
+						</ul>
+						<ul class="actions stacked">
+							<li><a href="#" class="button primary fit">Crear cuenta</a></li>
+							<li><a href="#" class="button fit">Iniciar sesion</a></li>
+						</ul>
+					</nav>
+
+				<!-- Banner -->
+				<!-- Note: The "styleN" class below should match that of the header element. -->
+					<section id="banner" class="style2">
+						<div class="inner">
+							<span class="image">
+								<img src="images/general.jpg" alt="foto general" />
+							</span>
+							<header class="major">
+								<h1>Catalogo general</h1>
+							</header>
+							<div class="content">
+								<p></p>
+							</div>
+						</div>
+					</section>
+
+						<!-- Two -->
+							<section id="two" class="spotlights">
+								<section>
+									<a class="image">
+										<img src="images/buzo1.jpg" alt="buzo mujer 1" data-position="center center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Sinergia</h3>
+											</header>
+											<p>$8.500</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzoH1.jpg" alt="buzo hombre 1" data-position="top center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Animal rose</h3>
+											</header>
+											<p>$6.500</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzo2.jpg" alt="buzo mujer 2" data-position="25% 25%" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Animal white</h3>
+											</header>
+											<p>$6.500</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzoH2.jpg" alt="buzo hombre 2" data-position="top center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Rhoncus magna</h3>
+											</header>
+											<p>$7.000</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzo3.jpg" alt="buzo mujer 3" data-position="25% 25%" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Sed nunc ligula</h3>
+											</header>
+											<p>$7.000</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzoH3.jpg" alt="buzo hombre 3" data-position="top center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Thrasher</h3>
+											</header>
+											<p>$8.500</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzo4.jpg" alt="buzo mujer 4" data-position="center center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Giraffe</h3>
+											</header>
+											<p>$6.500</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzoH4.jpg" alt="buzo hombre 4" data-position="top center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Kept</h3>
+											</header>
+											<p>$9.000</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzo5.jpg" alt="buzo mujer 5" data-position="25% 25%" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Sed nunc ligula</h3>
+											</header>
+											<p>$7.500</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzoH5.jpg" alt="buzo hombre 5" data-position="top center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Rhoncus magna</h3>
+											</header>
+											<p>$7.000</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzo6.jpg" alt="buzo mujer 6" data-position="25% 25%" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Sed nunc ligula</h3>
+											</header>
+											<p>$7.000</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+								<section>
+									<a class="image">
+										<img src="images/buzoH6.jpg" alt="buzo hombre 6" data-position="top center" />
+									</a>
+									<div class="content">
+										<div class="inner">
+											<header class="major">
+												<h3>Rhoncus magna</h3>
+											</header>
+											<p>$9.000</p>
+											<ul class="actions">
+												<li><a href="contacto.php" class="button">COMPRAR</a></li>
+											</ul>
+										</div>
+									</div>
+								</section>
+							</section>
+						
+				<!-- Footer -->
+				<footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+							<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>&copy; Design: by Surreal </li>
+						</ul>
+					</div>
+				</footer>
+
+				</div>
+
+				
+			<!-- Scripts -->
+				<script src="assets/js/jquery.min.js"></script>
+				<script src="assets/js/jquery.scrolly.min.js"></script>
+				<script src="assets/js/jquery.scrollex.min.js"></script>
+				<script src="assets/js/browser.min.js"></script>
+				<script src="assets/js/breakpoints.min.js"></script>
+				<script src="assets/js/util.js"></script>
+				<script src="assets/js/main.js"></script>
+
+		</body>
+	</html>
