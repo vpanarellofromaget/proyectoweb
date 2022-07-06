3401c46f7868951c841e269ce14cd200395fcebb
valeriva44 <106684206+valeriva44@users.noreply.github.com>
2022-07-06 19:16:04 -0300

    Add files via upload

--- informacion.php
+++ informacion.php
@@ -95,7 +95,7 @@
 						<div class="contact-method">
 							<span class="icon solid alt fa-envelope"></span>
 							<h3>Email</h3>
-							<a href="#">nuestro mail</a>
+							<a>surreal@gmail.com</a>
 						</div>
 					</section>
 						<section>
@@ -121,11 +121,11 @@
 			<footer id="footer">
 				<div class="inner">
 					<ul class="icons">
-						<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-						<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+						<li><a class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+						<li><a sclass="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
 					</ul>
 					<ul class="copyright">
 						<li>&copy; Design: by Surreal</li>
